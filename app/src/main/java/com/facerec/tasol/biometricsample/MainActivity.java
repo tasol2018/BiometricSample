d269ce3c9c720352288cfa90d7577c40170cd659
pratik vyas <pratik.vyas@tasolglobal.com>
2018-07-06 11:45:31 +0530

    Initial commit

---
+++ app/src/main/java/com/facerec/tasol/biometricsample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.facerec.tasol.biometricsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
