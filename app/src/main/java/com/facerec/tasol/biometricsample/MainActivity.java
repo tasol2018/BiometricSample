c0a117acea9c922d349a6967d650035ebff79fbe
pratik vyas <pratik.vyas@tasolglobal.com>
2018-07-06 11:57:07 +0530

    added the biometric library

--- app/src/main/java/com/facerec/tasol/biometricsample/MainActivity.java
+++ app/src/main/java/com/facerec/tasol/biometricsample/MainActivity.java
@@ -3,11 +3,14 @@ package com.facerec.tasol.biometricsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.integratedbiometrics.ibscanultimate.IBScan;
 
+public class MainActivity extends AppCompatActivity {
+    IBScan m_ibScan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m_ibScan = IBScan.getInstance(MainActivity.this);
     }
 }
